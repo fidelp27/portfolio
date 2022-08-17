a055ad1ab146b18424f41ee9dfcef24f1487b5be
fidelp27 <fidel2702@gmail.com>
2022-08-17 07:09:43 -0300

    Updates

--- js/data.js
+++ js/data.js
@@ -9,23 +9,6 @@ const projects = [
     skills: 'React, Redux, Skeleton, Peticiones HTTP, SweetAlert2, Formik',
   },
   {
-    name: 'Arte+ ',
-    description:
-      'Proyecto colaborativo hecho con compañeros de Coderhouse. Web responsive para mostrar productos y generar contacto con clientes',
-    img: 'https://i.imgur.com/i8ZRKc4.png',
-    video: '',
-    uri: 'https://gabrielaberdasco.github.io/ArteM-s/index.html',
-    skills: 'HTML, CSS, JS',
-  },
-  {
-    name: 'BatataBit',
-    description: 'Sitio web estático para mostrar información de la empresa',
-    img: 'https://i.imgur.com/Zms77gx.png',
-    video: '',
-    uri: 'https://priceless-cray-231fa1.netlify.app/',
-    skills: 'HTML, CSS',
-  },
-  {
     name: 'Wild Wave Surf',
     description:
       'Proyecto colaborativo hecho con compañeros en la aceleradora de empleos NoCountry. Tienda con plataforma de pago (pay pal)',
@@ -35,6 +18,16 @@ const projects = [
     skills: 'React, CSS, Peticiones HTTP',
   },
   {
+    name: 'Arte+ ',
+    description:
+      'Proyecto colaborativo hecho con compañeros de Coderhouse. Web responsive para mostrar productos y generar contacto con clientes',
+    img: 'https://i.imgur.com/i8ZRKc4.png',
+    video: '',
+    uri: 'https://gabrielaberdasco.github.io/ArteM-s/index.html',
+    skills: 'HTML, CSS, JS',
+  },
+
+  {
     name: 'Z-Games',
     description:
       'Mi primer proyecto hecho con HTML, CSS y JS. Fue el proyecto final del curso de JS en Coderhouse. El código es un desastre pero le tengo cariño ',
@@ -42,6 +35,14 @@ const projects = [
     video: '',
     uri: 'https://pensive-euler-5b02e3.netlify.app/',
     skills: 'HTML, CSS, JS',
+  },
+  {
+    name: 'BatataBit',
+    description: 'Sitio web estático para mostrar información de la empresa',
+    img: 'https://i.imgur.com/Zms77gx.png',
+    video: '',
+    uri: 'https://priceless-cray-231fa1.netlify.app/',
+    skills: 'HTML, CSS',
   },
 ];
 
